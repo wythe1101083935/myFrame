221da2d4ee528b72fb0b9db2489977282d889cbf
wzh <1101083935@qq.com>
2018-07-11 15:46:43 +0800

    读写分离

--- wythe/data/db/Mysql.php
+++ wythe/data/db/Mysql.php
@@ -26,27 +26,30 @@ class Mysql{
         // 数据库调试模式
         'debug'           => false,
         // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-        'deploy'          => false,
+        //'deploy'          => false,
         // 数据库读写是否分离 主从式有效
         'rw_separate'     => false,
+        'readServer'=>[
+        	'hostname'=> '127.0.0.1','database' => 'knowlege','username' => 'root','password' => '123456','hostport' => '3306','params' => [],'charset'=> 'utf8','rate'=>1,
+        	'hostname'=> '127.0.0.1','database' => 'knowlege','username' => 'root','password' => '123456','hostport' => '3306','params' => [],'charset'=> 'utf8','rate'=>1,
+        	'hostname'=> '127.0.0.1','database' => 'knowlege','username' => 'root','password' => '123456','hostport' => '3306','params' => [],'charset'=> 'utf8','rate'=>1,
+        ],
         // 读写分离后 主服务器数量
-        'master_num'      => 1,
+        //'master_num'      => 1,
         // 指定从服务器序号
-        'slave_no'        => '',
+        //'slave_no'        => '',
         // 是否严格检查字段是否存在
-        'fields_strict'   => true,
+        //'fields_strict'   => true,
         // 数据返回类型
         'result_type'     => PDO::FETCH_ASSOC,
         // 数据集返回类型
         'resultset_type'  => 'array',
         // 自动写入时间戳字段
-        'auto_timestamp'  => false,
+        //'auto_timestamp'  => false,
         // 时间字段取出后的默认时间格式
-        'datetime_format' => 'Y-m-d H:i:s',
-        // 是否需要进行SQL性能分析
-        'sql_explain'     => false,
+        //'datetime_format' => 'Y-m-d H:i:s',
         // 是否需要断线重连
-        'break_reconnect' => false,
+        //'break_reconnect' => false,
 	];
 
 	/*数据库连接实例-读*/
@@ -83,27 +86,39 @@ class Mysql{
 
 	/*数据库连接*/
 	protected function connect(){
-		$config = $this->config;
 		/*设置写服务器*/
-		if(is_null($this->writeLink)){
-			/*获得DSN*/
-			$dsn = 'mysql:host=' . $config['hostname'] . ';port=' . $config['hostport'] . ';dbname=' . $config['database'];
-			if(!empty($config['charset'])){
-				$dsn .= ';charset=' . $config['charset'];
-			}
-			/*数据库连接*/	
-			$this->writeLink = new \PDO($dsn,$config['username'],$config['password']);
+		if(is_null($this->writeLink)){//写服务器只有这一台
+			$this->writeLink = $this->createPDO($this->config);	
 		}
-		/*设置读服务器*/
-		if(is_null($this->readLink)){	
-			if($config['rw_separate']){
-
+		if(is_null($this->readLink)){
+			if($this->config['rw_separate']){/*如果是读写分离*/
+				$total = 0;
+				foreach ($this->config['readServer'] as $val) {
+					$total += $val['rate'];
+				}
+				$rand = random_int(0,$total-1);
+				foreach ($this->config['readServer'] as $key => $val) {
+					$rand -= $val['rate'];
+					if($rand<0){
+						$sign = $key;
+						break;
+					}
+				}
+				$readLink = $this->createPDO($this->config['readServer'][$sign]);
 			}else{
 				$this->readLink = $this->writeLink;
 			}
 		}
-
 	}	
+
+	/*pdo连接数据库*/
+	protected function createPDO($conf){
+		$dsn = 'mysql:host=' . $conf['hostname'] . ';port=' . $conf['hostport'] . ';dbname=' . $conf['database'];
+		if(!empty($config['charset'])){
+			$dsn .= ';charset=' . $conf['charset'];
+		}
+		return new \PDO($dsn,$conf['username'],$conf['password']);
+	}
 
 	/*执行查询函数*/
 	public function query($sql,$bind=[],$type="query"){
